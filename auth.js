5237b4adb86b97aeb862903ded38641e2bda261f
Ismail <vinux181@gmail.com>
2021-12-14 13:38:50 +0100

    Update auth.js
    changing response status from 400 to 401 Unauthorized

--- auth.js
+++ auth.js
@@ -23,7 +23,7 @@ module.exports = (router) => {
   router.post('/login', (req, res) => {
     passport.authenticate('local', { session: false }, (error, user, info) => {
       if (error || !user) {
-        return res.status(400).json({
+        return res.status(401).json({
           message: 'Something is not right',
           user: user
         });
