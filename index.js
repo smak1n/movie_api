d6a661a24b6c7ba7786129e6c8d9d9baf86d61b1
Ismail <vinux181@gmail.com>
2021-12-15 18:28:46 +0100

    update index.js

--- index.js
+++ index.js
@@ -152,12 +152,27 @@ app.post('/users',
     });
 });
 
-// Allow users to update their user info (by username)
-app.put('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
+// Allow users to update their user info
+app.put('/users/:Username', passport.authenticate('jwt', { session: false }),
+[
+  check('Username', 'Username is required').isLength({min: 5}),
+  check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+  check('Password', 'Password is required').not().isEmpty(),
+  check('Email', 'Email does not appear to be valid').isEmail()
+], (req, res) => {
+
+  // check the validation object for errors
+  let errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  let hashedPassword = Users.hashPassword(req.body.Password);
   Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
     {
       Username: req.body.Username,
-      Password: req.body.Password,
+      Password: hashedPassword,
       Email: req.body.Email,
       Birthday: req.body.Birthday
     }
